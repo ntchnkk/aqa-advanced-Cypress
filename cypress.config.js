fd034ee6d6fc92ecda250bbb4dc9539eaf98c02e
ntchnkk <katrusyanazarenko@gmail.com>
2025-10-25 22:56:29 -0400

    HW_19.1: add Cypress queries for header and footer buttons

--- cypress.config.js
+++ cypress.config.js
@@ -2,6 +2,7 @@ const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
   e2e: {
+    baseUrl: 'https://guest:welcome2qauto@qauto.forstudy.space',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
