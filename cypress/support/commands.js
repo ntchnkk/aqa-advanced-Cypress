bf6a8334e48b58f1110a4dc65df9e4a1dd001aca
ntchnkk <katrusyanazarenko@gmail.com>
2025-11-02 21:14:18 -0500

    add Cypress_assertions folder with HW_20.1

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -23,3 +23,26 @@
 //
 // -- This will overwrite an existing command --
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("login", (email, password) => {
+  cy.visit("/");
+  cy.contains("Sign In").click();
+  cy.get("#signinEmail").type(email);
+  cy.get("#signinPassword").type(password, { sensitive: true });
+  cy.contains("Login").click();
+});
+
+Cypress.Commands.overwrite("type", (originalFn, element, text, options) => {
+  if (options && options.sensitive) {
+    // turn off original log
+    options.log = false;
+    // create our own log with masked message
+    Cypress.log({
+      $el: element,
+      name: "type",
+      message: "*".repeat(text.length),
+    });
+  }
+
+  return originalFn(element, text, options);
+});
